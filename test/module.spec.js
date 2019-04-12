7e51749980be3b406d3d0fb68398664fa5823472
Hanut Singh Gusain <hanutsingh@gmail.com>
2019-04-12 17:08:32 +0530

    Project scaffold

---
+++ test/module.spec.js
@@ -0,0 +1,16 @@
+const expect = require('chai').expect;
+
+const teamID = "DEF123GHIJ";
+const keyID = "ABCD1234";
+const keyValue = "elemenohpee";
+
+
+
+describe('Test Suit for apple-push module', function() {
+
+	it('should contain a createToken method', function() {
+		
+	})
+
+
+});
